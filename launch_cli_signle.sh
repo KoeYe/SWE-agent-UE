98bba28049bb0f4ac89074aa5c5b455641f1eeea
koe <xiaokang.ye@outlook.com>
2025-09-15 07:06:04 +0000

    feat: base64 image compression; feat: unreal-mcp intergration

--- launch_cli_signle.sh
+++ launch_cli_signle.sh
@@ -1,3 +1,3 @@
 sweagent run \
     --config=./config/unreal_coding.yaml \
-    --problem_statement.text="Tell me what you can do with Unreal MCP"
+    --problem_statement.text="tell me what you can see"
